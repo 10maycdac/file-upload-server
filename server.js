af741120564594fb29a9346f76d01f79cb6da00e
10maycdac <55127938+10maycdac@users.noreply.github.com>
2020-03-20 13:25:53 +0530

    xls to json

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
+const parser = new (require('simple-excel-to-json').XlsParser)();
 
 const app = express();
 
 app.use(fileUpload());
 
+let receivedFileName = "";
 // Upload Endpoint
 app.post('/upload', (req, res) => {
     if (req.files === null) {
@@ -12,7 +14,8 @@ app.post('/upload', (req, res) => {
     }
 
     const file = req.files.file;
-
+    receivedFileName = "./uploads/" + file.name;
+    console.log(receivedFileName);
     file.mv(`${__dirname}/uploads/${file.name}`, err => {
         if (err) {
             console.error(err);
@@ -20,6 +23,13 @@ app.post('/upload', (req, res) => {
         }
 
         res.json({ fileName: file.name, filePath: `/uploads/${file.name}` });
+
+
+        //kadu's code
+
+        let doc = parser.parseXls2Json(receivedFileName);
+        let productArray = doc[0];
+        console.log(productArray);
     });
 });
 
